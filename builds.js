009b37a93ef616c37a2c1bf085ca77f029706bce
Tyler <hjy4kh@virginia.edu>
2024-04-24 15:50:22 -0400

    added dps calculation for physical damage

--- builds.js
+++ builds.js
@@ -39,3 +39,35 @@ function loadBuild() {
         }
     });
 }
+
+// formula: Damage = (physicalDamage + criticalChance * (modifier - 1) * 100) + onhitphysicalDamage
+// mitigated damage = damage x 100/(100 + armor * armorPen - lethality)
+// DPS = Damage * attackspeed
+function calculateDps() {
+    var stats = {};
+
+    // Retrieve user input stats
+    $("#userStatsForm").children().each(function(index, element) {
+        var statName = $(element).find('label').text().trim(); // Get the label text as the stat name
+        var statValue = parseFloat($(element).find('input').val()); // Get the input value as the stat value
+        stats[statName] = statValue; 
+    });
+
+    // Extract individual stats from the stats object
+    var physicalDamage = stats['Attack Damage'];
+    var criticalChance = stats['Critical Strike Chance'];
+    var modifier = stats['Ability Power'];
+    var onhitPhysicalDamage = stats['On-Hit Physical Damage'];
+    var armor = stats['Armor'];
+    var armorPenetration = stats['Armor Penetration'];
+    var lethality = stats['Lethality'];
+    var attackSpeed = stats['Attack Speed'];
+
+    var damage = (physicalDamage + criticalChance * (modifier - 1) * 100) + onhitPhysicalDamage;
+
+    var mitigatedDamage = damage * 100 / (100 + armor * armorPenetration - lethality);
+
+    var dps = mitigatedDamage * attackSpeed;
+
+    return dps;
+}
